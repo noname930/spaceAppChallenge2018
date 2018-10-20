d19b9e1c7f96da61b0632266d4e1c5bdfe194838
Alessio Spina <alessiospina@Alessio-Spina-MAC.local>
2018-10-20 17:40:53 +0200

    v0.01

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform Target;
+
+    [SerializeField]
+    private float xmin;
+
+    [SerializeField]
+    private float xmax;
+
+    [SerializeField]
+    private float ymin;
+
+    [SerializeField]
+    private float ymax;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+
+	
+	// Update is called once per framed
+	void Update () {
+
+    }
+}
---
+++ Assets/moveTo_Y.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTo_Y : MonoBehaviour {
+
+    private float speed = 3f;
+    private float height = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Vector2 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY) * height;
+
+
+    }
+}
